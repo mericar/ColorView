6e634c760b5d0f6cf7960797ca89732090e87a7d
mirkmasterC <mirkocarich@gmail.com>
2016-01-26 17:18:12 -0800

    Initial Commit

---
+++ ColorView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorView
+//
+//  Created by M on 2016-01-26.
+//  Copyright © 2016 mec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
